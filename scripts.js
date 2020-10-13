70c632685ebda390d0e1725fe867932a7b3bc552
Bekhzod Akbarov <stx.webdev@gmail.com>
2020-10-14 00:29:29 +0500

    Add selected seats to localStorage in updateSelectedCount function and create a function to save movie index as well movie price in localStorage

--- scripts.js
+++ scripts.js
@@ -5,9 +5,23 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = parseInt(movieSelect.value);
 
+// * Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // * Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  // ** Copy selected seats into arr
+  // ** Map through array
+  // ** return a new array indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = ticketPrice * selectedSeatsCount;
@@ -16,6 +30,7 @@ function updateSelectedCount() {
 // * Movie select event
 movieSelect.addEventListener('change', ({ target }) => {
   ticketPrice = +target.value;
+  setMovieData(target.selectedIndex, target.value);
   updateSelectedCount();
 });
 
